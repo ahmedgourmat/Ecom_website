adc3c474dd426ad7823791241ee117722836cc9e
raouf-kri <raoufkh05000@gmail.com>
2024-08-28 23:51:37 +0100

    cart modifs

--- Client/src/Pages/Cart.tsx
+++ Client/src/Pages/Cart.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
-import { CartList } from "../Components/Cart/Cartlist";
+import { CartList } from "../Components/Cart/CartList";
 import { CartTotal } from "../Components/Cart/CartTotal";
 import { Coupon } from "../Components/Cart/Coupon";
 import { useState } from "react";
@@ -8,6 +8,15 @@ export const Cart: React.FC = () => {
   const [subtotal, setSubtotal] = useState(0);
   const shipping = 50;
   const total = subtotal + shipping;
+
+  // State to determine if the cart is empty
+  const [isCartEmpty, setIsCartEmpty] = useState(false);
+
+  // Callback function to update if the cart is empty based on the subtotal
+  const handleSubtotalChange = (newSubtotal: number) => {
+    setSubtotal(newSubtotal);
+    setIsCartEmpty(newSubtotal === 0);
+  };
 
   return (
     <Box
@@ -18,11 +27,14 @@ export const Cart: React.FC = () => {
       mt="40px"
       mb="40px"
     >
-      <CartList onSubtotalChange={setSubtotal} />
-      <Box display="flex" w="80%" justifyContent="space-between">
-        <Coupon />
-        <CartTotal subtotal={subtotal} shipping={shipping} total={total} />
-      </Box>
+      <CartList onSubtotalChange={handleSubtotalChange} />
+      
+      {!isCartEmpty && ( // Conditionally render Coupon and CartTotal if the cart is not empty
+        <Box display="flex" w="80%" justifyContent="space-between">
+          <Coupon />
+          <CartTotal subtotal={subtotal} shipping={shipping} total={total} />
+        </Box>
+      )}
     </Box>
   );
 };
